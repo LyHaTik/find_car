3ee3eda6f2846d9e3f2ae7cd04519ea46fef31c2
LyHaTik <96049450+LyHaTik@users.noreply.github.com>
2024-07-26 17:39:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,26 +2,13 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
 let order = document.getElementById("order");
-
-btn1.addEventListener("click", function(){
-	document.getElementById("main").style.display = "none";
-	document.getElementById("form").style.display = "block";
-});
-
-btn2.addEventListener("click", function(){
-	document.getElementById("main").style.display = "none";
-	document.getElementById("form").style.display = "block";
-});
 
 order.addEventListener("click", () => {
 	let name = document.getElementById("username").value;
 	let email = document.getElementById("user_email").value;
 	let phone = document.getElementById("user_phone").value;
 	let comment = document.getElementById("comment").value;
-	let dacoin = "True";
 	if(name.length < 4) {
 		document.getElementById("error").innerText = ".. Ошибка в имени ..";
 		return;
@@ -40,7 +27,6 @@ order.addEventListener("click", () => {
 		email: email,
 		phone: phone,
 		comment: comment,
-		dacoin: dacoin
 	}
 	tg.sendData(JSON.stringify(data));
 	tg.close;
