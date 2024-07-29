e29885c1bcc7931a65eb0637b34725809e07ea9c
LyHaTik <96049450+LyHaTik@users.noreply.github.com>
2024-07-29 12:46:43 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ btnclose.addEventListener("click", () => {
 	let category = document.getElementById("category").value;
 	let passengers = document.getElementById("passengers").value;
 	let count = document.getElementById("count").value;
+	let type_ts = "bike"
 
 	let data = {
+		type_ts: type_ts,
 		category: category,
 		passengers: passengers,
 		count: count,
